9d2e9dd866d07c00de4562bc8a6e3d8f72f70b59
よね/Yone <git@yoneyo.com>
2024-03-29 02:28:47 +0900

    Change: コピーライト表記の変更

--- src/ydits_twitter/__init__.py
+++ src/ydits_twitter/__init__.py
@@ -14,4 +14,4 @@ from .__main__ import YditsTwitter
 __title__ = "YDITS for Twitter"
 __version__ = "3.0.0"
 __author__ = "よね/Yone"
-__copyright__ = "Copyright (c) YDITS, よね/Yone"
+__copyright__ = "Copyright (c) よね/Yone"
