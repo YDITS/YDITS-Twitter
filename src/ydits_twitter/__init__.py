544b0e3c9c02ea1c025147eb457c9d078a8ffa6f
よね/Yone <git@yoneyo.com>
2024-09-04 02:39:12 +0900

    Add: `src/ydits_twitter/__init__.py` 定数の追加

--- src/ydits_twitter/__init__.py
+++ src/ydits_twitter/__init__.py
@@ -13,5 +13,8 @@ from .__main__ import YditsTwitter
 
 __title__ = "YDITS for Twitter"
 __version__ = "3.0.0"
+__description__ = "X (Twitter) bot providing disaster prevention information"
+__description_jp__ = "防災情報を提供する X (Twitter) Bot"
 __author__ = "よね/Yone"
 __copyright__ = "Copyright (C) よね/Yone"
+__license__ = "Apache License 2.0"
